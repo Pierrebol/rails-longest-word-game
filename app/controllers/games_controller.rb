ec50e533d76929c595731702d8f71db056494479
pierrebollack <pierre.bollack@edhec.com>
2021-06-07 15:38:43 +0200

    longest word

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0...10).map { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def english_word?(attempt)
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+    word_serialized = URI.open(url).read
+    JSON.parse(word_serialized)
+  end
+
+  def score
+    @result = 'It is not in the grid'
+    @attempt_letters = params[:word].upcase.split('')
+    @letters = params[:letters]
+    if @attempt_letters.all? { |letter| @attempt_letters.count(letter) <= @letters.count(letter) }
+      if english_word?(params[:word])['found'] == true
+        @result = 'Well Done!'
+      else @result = 'It is not an english word'
+      end
+      @result
+    end
+  end
+end
